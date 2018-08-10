c93e05b305f51f16c2844cff2159cfb28e25fef1
Shajan Jacob <shajanjp@gmail.com>
2018-08-10 18:52:47 +0530

    fixes store model and validation

--- app/stores/libraries/stores.server.validation.js
+++ app/stores/libraries/stores.server.validation.js
@@ -1,9 +1,23 @@
 const joi = require('joi');
-// const mongoId = joi.string().length(24);
+const mongoId = joi.string().length(24);
+
+const addressSchema = require('../../users/libraries/users.server.validation.js').addressSchema;
 
 const storeInsertSchema = joi.object().keys({
-
-  title: joi.string().allow('').optional(),
+  title: joi.string().max(128).allow('').optional().default(''),
+  about: joi.string().max(128).allow('').optional().default(''),
+  category: joi.array().items(joi.string().max(32).optional()).optional().default([]),
+  owners: joi.array().items(mongoId).optional().default([]),
+  address: addressSchema,
+  open: joi.object().keys({
+    day: joi.number(),
+    time: joi.number(),
+  }),
+  logo: joi.string().max(128).allow('').optional().default(''),
+  photos: joi.array().items(joi.string()).optional().default([]),
+  phone: joi.string().max(128).allow('').optional().default(''),
+  website: joi.string().max(128).allow('').optional().default(''),
+  email: joi.string().max(128).allow('').optional().default(''),
 });
 
 exports.validateInsertStore = function(req, res, next) {
--- app/stores/models/stores.server.model.js
+++ app/stores/models/stores.server.model.js
@@ -1,8 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const addressSchema = require('../../users/models/address.server.model.js').addressSchema;
 
 let storeSchema = new Schema({
   title: String,
+  about: String,
+  category: [String],
+  owners: [{
+    type: Schema.ObjectId,
+    ref: 'user',
+  }],
+  address: addressSchema,
+  open: {
+    day: Number,
+    time: Number,
+  },
+  logo: String,
+  photos: [String],
+  phone: String,
+  website: String,
+  email: String,
 });
 
 mongoose.model('store', storeSchema);
